f9e67b63cad7e20997d047dfd4413c17036bfa3d
HiepTrinh <xmikecolx@gmail.com>
2022-09-13 20:57:26 -0500

    Add an init function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-const board = document.querySelector('.game-board');
 
 const row = 6;
 const column = 7;
+
+const board = document.querySelector('.game-board');
+
+init();
 
 function init() {
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < column; j++) {
       const divElement = document.createElement('div');
+      divElement.classList.add('cell');
+      board.appendChild(divElement);
     }
   }
 }
