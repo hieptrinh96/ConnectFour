0c608abbb1101119ff0ec2b47c737311443c4bb1
HiepTrinh <xmikecolx@gmail.com>
2022-09-13 19:01:09 -0500

    Create init function

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const board = document.querySelector('.game-board');
+
+const row = 6;
+const column = 7;
+
+function init() {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      const divElement = document.createElement('div');
+    }
+  }
+}
