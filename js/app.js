04c85dfe04d8c407e2de636333315d7da76f1b70
HiepTrinh <xmikecolx@gmail.com>
2022-09-14 17:44:45 -0500

    Fixing bugs

--- js/app.js
+++ js/app.js
@@ -1,55 +1,82 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const row = 6;
-const column = 7;
-const arrOfCoords = [];
+const rows = 6;
+const columns = 7;
+const rowArr = [];
+
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-let winner = false;
+let gameBoard;
 let playerOne = 'red';
 let turn = playerOne;
 let playerTwo = 'yellow';
-
+let gameComplete = false;
+let lastColumn = [];
 /*------------------------ Cached Element References ------------------------*/
 
 const board = document.querySelector('.game-board');
 const cells = document.getElementsByClassName('cell');
+const winner = document.getElementById('winner');
 const message = document.getElementById('message')
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 /*-------------------------------- Functions --------------------------------*/
-
-
-// create an init function that will create the container board div
-// set a row and column variable to create the grid layout
-// set the variables such as board, playerOne, playerTwo, Winner
-// create a function to 
-
 init();
-console.log(arrOfCoords.length)
 function init() {
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < column; j++) {
+  gameBoard = [];
+  lastColumn = [5, 5, 5, 5, 5, 5, 5]
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      rowArr.push(' ');
       const divElement = document.createElement('div');
-      divElement.classList.add('cell');
       divElement.id = `${i}, ${j}`;
-      arrOfCoords.push(divElement.id)
+      divElement.classList.add('cell');
       divElement.addEventListener('click', renderCircles)
       board.appendChild(divElement);
     }
   }
+  gameBoard.push(rowArr)
 }
-//have the pieces show up on the board and change the turn
-// once we click on a circle, what do we want it to do
+
 function renderCircles() {
+  if (winner) return;
+
+  let pairs = this.id.split(',');
+  let row = parseInt(pairs[0]);
+  let column = parseInt(pairs[1]);
+  r = lastColumn[column];
+
+  if (row < 0) return;
+  gameBoard[row][column] = turn;
+  const puck = document.getElementById(`${row}, ${column}`);
   if (turn === playerOne) {
-    this.classList.add('red');
+    puck.classList.add('red');
     turn = playerTwo;
   }
-  else if (turn === playerTwo) {
-    this.classList.add('yellow');
+  else {
+    puck.classList.add('yellow');
     turn = playerOne;
   }
+  row -= 1;
+  winningCombos();
+}
+
+function winningCombos() {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns - 3; j++) {
+      if (gameBoard[i][j] !== ' ') {
+        if (gameBoard[i][j] === gameBoard[i + 1][j + 1] && gameBoard[i + 2][j + 2] === gameBoard[i + 3][j + 3]) {
+          getWinner(i, j);
+          return;
+        }
+      }
+    }
+  }
+}
+
+function getWinner(i, j) {
+  if (gameBoard[i][j] === playerOne) winner.textContent = 'Player One Wins!';
+  else winner.textContent = 'Player Two Wins!';
+  gameComplete = true;
 }
